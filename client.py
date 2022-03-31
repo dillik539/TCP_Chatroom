f70c2ca30090291684dbb3bd365a4c18041e1105
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2022-03-31 18:05:35 -0500

    Added some method in client.py

--- client.py
+++ client.py
@@ -8,3 +8,32 @@ client_address = (host, port_Number)
 client = socket(AF_INET, SOCK_STREAM)
 client.connect(client_address)
 
+name = input('Enter your name: ')
+
+
+def receive_message():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            if message == 'NAME':
+                client.send(name.encode())
+            else:
+                print(message)
+        except:
+            print('An error occurred!')
+            client.close()
+            break
+
+
+def write_message():
+    while True:
+        chat_message = f'{name}: {input()}'
+        client.send(chat_message.encode())
+
+
+if __name__ == '__main__':
+
+    receive_message_thread = threading.Thread(target=receive_message)
+    write_message_thread = threading.Thread(target=write_message)
+    receive_message_thread.start()
+    write_message_thread.start()
